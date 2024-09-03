5e6f993fb4ffd578e82eaa7c4a05ece0e43a1f8b
Diederick de Buck <diederick.de.buck@platform-42.com>
2024-09-03 13:05:15 +0200

    dddd

---
+++ Sources/P42-keychain/Keychain.swift
@@ -0,0 +1,45 @@
+//
+//  Keychain.swift
+//  PreRelease
+//
+//  Created by Diederick de Buck on 04/11/2022.
+//
+
+import Foundation
+
+
+final class Keychain {
+    
+    static let instance = Keychain()
+    
+    func getKeyChain(service: String, account: String) -> Data? {
+        let query = [
+            kSecReturnData: true,
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrService: service,
+            kSecAttrAccount: account
+        ] as CFDictionary
+        var result: AnyObject?
+        SecItemCopyMatching(query, &result)
+        return (result as? Data)
+    }
+    
+    func addToKeyChain(secret: Data, service: String, account: String) -> Int32 {
+        let query = [
+            kSecValueData: secret,
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrService: service,
+            kSecAttrAccount: account
+        ] as CFDictionary
+        return SecItemAdd(query, nil)
+    }
+    
+    func delFromKeyChain(service: String, account: String) {
+        let query = [
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrService: service,
+            kSecAttrAccount: account
+        ] as CFDictionary
+        SecItemDelete(query)
+    }
+}
