244c0a9db559ca749ea0478638ac415ccba1d7d6
Diederick <diederick.de.buck@platform-42.com>
2025-12-15 15:04:33 +0100

    added security function wrappers around keychain

---
+++ Sources/P42-keychain/Security.swift
@@ -0,0 +1,42 @@
+//
+//  Security.swift
+//  P42-keychain
+//
+//  Created by Diederick de Buck on 15/12/2025.
+//
+import Foundation
+
+public class Security {
+    
+    public static func authorizeProfile(
+        _ email: String,
+        applicationName: String,
+        serviceToken: String,
+        secret: String
+    ) -> Bool {
+        let service = String(format: "%@.%@", applicationName, serviceToken)
+        Keychain.instance.delFromKeyChain(
+            service: service,
+            account: email
+        )
+        let status = Keychain.instance.addToKeyChain(
+            secret: Data(secret.utf8),
+            service: service,
+            account: email
+        )
+        return (status == 0)
+    }
+    
+    public static func revokeProfile(
+        _ email: String,
+        applicationName: String,
+        serviceToken: String
+    ) {
+        let service = String(format: "%@.%@", applicationName, serviceToken)
+        Keychain.instance.delFromKeyChain(
+            service: service,
+            account: email
+        )
+    }
+    
+}
