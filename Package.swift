a37751f6579d8b720de12c3ed1ed923a5aec194e
Diederick <diederick.de.buck@platform-42.com>
2025-12-23 08:40:37 +0100

    ssss

--- Package.swift
+++ Package.swift
@@ -3,6 +3,10 @@ import PackageDescription
 
 let package = Package(
     name: "P42-keychain",
+    platforms: [
+        .iOS(.v16),
+        .macOS(.v13)
+    ],
     products: [
         .library(
             name: "P42-keychain",
